bdc3234a1111b9b75d2f469e1c0c652efd3a094a
nik frank <nikbfrank@gmail.com>
2015-06-18 23:03:26 +0300

    fn running

--- lijsp.js
+++ lijsp.js
@@ -6,11 +6,13 @@ var lij = {};
 
 // here put built-in library, load libraries to extend it with
 lij.cx = {
-    defun:function(pms, cx, pcx){
+    defun:function(cdr, cx, pcx){
 	// put a lijsp fn ['progn', pms] onto the pcx
+	return pcx[cdr[0]] = ['fn'].concat(cdr.slice(1));
     },
-    degfun:function(pms, cx, pcx){
+    degfun:function(cdr, cx, pcx){
 	// put a lijsp fn ['progn', pms] onto the lij.cx
+	return lij.cx[cdr[0]] = ['fn'].concat(cdr.slice(1));
     },
 
     setfun:function(pms, cx, pcx){
@@ -22,10 +24,10 @@ lij.cx = {
 	return lij.cx[pms[0]] = pms[1];
     },
 
-    defmacro:function(pms, cx, pcx){
+    defmacro:function(cdr, cx, pcx){
 	// put a lijsp macro onto the pcx
     },
-    defgmacro:function(pms, cx, pcx){
+    defgmacro:function(cdr, cx, pcx){
 	// put a lijsp macro onto the lij.cx
     },
 
@@ -36,6 +38,26 @@ lij.cx = {
     setgmacro:function(pms, cx, pcx){
 	// put a js macro (fn) into the lij.cx
 	return lij.cx[pms[0]] = pms[1];
+    },
+
+    fn:function(pcdr, cx, pcx){
+	var mcx = cx, mpcx = pcx;
+	// run the function defined by pms
+	/*
+	  (defun multiply-by-seven (number)
+	  "Multiply NUMBER by seven."
+	  (* 7 number))
+	*/
+
+	// -> pcdr[0] is the list of params
+	// pcdr.slice(1) is an array of lists to sp in cx
+	// pcdr[end] is the returned value
+	return function(pms, cx, pcx){
+	    for(var i=pcdr[0].length; i-->0;) cx[pcdr[i]] = pms[i];
+	    var body = pcdr.slice(1);
+	    for(var i=0; i<body.length-1; ++i) lij.sp(body[i], cx, pcx);
+	    return lij.sp(body[body.length-1], cx, pcx);
+	};
     },
 
     '\'':function(pms, cx, pcx){
@@ -50,8 +72,7 @@ lij.cx = {
 
     date:function(pms, cx, pcx){
 	switch(pms.length){
-	case 0: return new Date; break;
-	case 1: return new Date(pms[0]); break;
+	case 0: return new Date; break; case 1: return new Date(pms[0]); break;
 	default: return new Date(pms[0],pms[1]||0,pms[2]||0,pms[3]||0,pms[4]||0,pms[5]||0,pms[6]||0);
 	}
     }
@@ -90,9 +111,15 @@ lij.sp = function(list, pcx, caller){
 	    }
 
 	}else if(otype(list[0]) === '[object String]'){
-	    return (typeof cx[list[0].split('(')[0]] === 'function')?
-		lij.sp([cx[list[0]]].concat(list.slice(1)), cx, list[0]):
-		[list[0]].concat(list.slice(1).map(lij.sp.bind({pcx:cx, caller:list[0]})));
+	    if(typeof cx[list[0].split('(')[0]] === 'function'){
+		return lij.sp([cx[list[0]]].concat(list.slice(1)), cx, list[0]);
+
+	    }else if(otype(cx[list[0].split('(')[0]]) === '[object Array]'){
+		// context var resolves to a list
+		// function or macro
+		return lij.sp([lij.sp(cx[list[0].split('(')[0]], cx, pcx)].concat(list.slice(1)), cx, pcx);
+
+	    }else return [list[0]].concat(list.slice(1).map(lij.sp.bind({pcx:cx, caller:list[0]})));
 
 	}else{
 	    // the first thing isn't sp able -- perhaps there could be plugins that handle these?
